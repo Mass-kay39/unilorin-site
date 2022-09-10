7bbdd01b476222eb8759699e770afda15849339f
Mass-kay39 <103989617+Mass-kay39@users.noreply.github.com>
2022-09-10 18:28:36 +0100

    first commit

---
+++ poral.js
@@ -0,0 +1,284 @@
+function clicFunction(){
+    document.getElementById('function').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let kenny = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < kenny.length; i++){
+     let hoverOn = kenny[i];
+     if(hoverOn.classList.contains('show')){
+        hoverOn.classList.remove('show')
+     }
+        }
+    }
+}
+function fifth(){
+    document.getElementById('fifth').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let taiwo = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < taiwo.length; i++){
+     let coverOn = taiwo[i];
+     if(coverOn.classList.contains('show')){
+        coverOn.classList.remove('show')
+     }
+        }
+    }
+}
+function third(){
+    document.getElementById('third').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let rash  = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < rash.length; i++){
+     let roverOn = rash[i];
+     if(roverOn.classList.contains('show')){
+        roverOn.classList.remove('show')
+     }
+        }
+    }
+}
+function fourth(){
+    document.getElementById('fourth').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let mymum = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; mymum.length; i++){
+     let moverOn = mymum[i];
+     if(moverOn.classList.contains('show')){
+        moverOn.classList.remove('show')
+     }
+        }
+    }
+}
+function second(){
+    document.getElementById('second').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let sec = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < sec.length; i++){
+     let soverOn = sec[i];
+     if(soverOn.classList.contains('show')){
+        soverOn.classList.remove('show')
+     }
+        }
+    }
+}
+function first(){
+    document.getElementById('first').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let fuse = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < fuse.length; i++){
+     let hoverOn = fuse[i];
+     if(hoverOn.classList.contains('show')){
+        hoverOn.classList.remove('show')
+     }
+        }
+    }
+}
+function openav(){
+    document.getElementById('mySidebar').style.width = '100%'
+}
+
+function closenav(){
+    document.getElementById('mySidebar').style.width = '0%'
+}
+
+window.onscroll = function(){
+    myFunction()
+}
+let topnav = document.getElementById('stick-nav');
+sticky = topnav.offsetTop;
+function myFunction(){
+    if(window.pageYOffset > sticky){
+        topnav.classList.add('sticky')
+    }
+    else{
+        topnav.classList.remove('sticky')
+    }
+}
+
+function jan(){
+    document.getElementById('jan').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let use = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < use.length; i++){
+     let On = use[i];
+     if(On.classList.contains('show')){
+        On.classList.remove('show')
+     }
+        }
+    }
+}
+
+
+
+function feb(){
+    document.getElementById('feb').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let use = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < use.length; i++){
+     let On = use[i];
+     if(On.classList.contains('show')){
+        On.classList.remove('show')
+     }
+        }
+    }
+}
+
+
+
+
+function march(){
+    document.getElementById('march').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let use = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < use.length; i++){
+     let On = use[i];
+     if(On.classList.contains('show')){
+        On.classList.remove('show')
+     }
+        }
+    }
+}
+
+
+
+
+function april(){
+    document.getElementById('april').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let use = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < use.length; i++){
+     let On = use[i];
+     if(On.classList.contains('show')){
+        On.classList.remove('show')
+     }
+        }
+    }
+}
+
+
+
+
+function may(){
+    document.getElementById('may').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let use = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < use.length; i++){
+     let On = use[i];
+     if(On.classList.contains('show')){
+        On.classList.remove('show')
+     }
+        }
+    }
+}
+
+
+
+
+function june(){
+    document.getElementById('june').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let use = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < use.length; i++){
+     let On = use[i];
+     if(On.classList.contains('show')){
+        On.classList.remove('show')
+     }
+        }
+    }
+}
+
+
+
+
+function july(){
+    document.getElementById('july').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let use = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < use.length; i++){
+     let On = use[i];
+     if(On.classList.contains('show')){
+        On.classList.remove('show')
+     }
+        }
+    }
+}
+
+
+
+
+function aug(){
+    document.getElementById('aug').classList.toggle('show');
+}
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let use = document.getElementsByClassName('dropdwn-content');
+        let i;
+        for(i = 0; i < use.length; i++){
+     let On = use[i];
+     if(On.classList.contains('show')){
+        On.classList.remove('show')
+     }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+function showt(){
+    document.getElementById('sidemenu').style.width = '100%'
+}
+function hiddent(){
+    document.getElementById('sidemenu').style.width = '0%'
+}
+ let img = document.getElementById('slides');
+ imgArray = ['first.jpeg', 'second.jpeg', 'third.jpeg', 'fourth.jpeg'];
+ imgIndex = 0
+ function slideshow(){
+    img.setAttribute('src', imgArray[imgIndex]);
+    imgIndex++
+    if(imgIndex >= imgArray.length){
+    imgIndex = 0;}
+ }
+ setInterval(slideshow, 4000)
